9574a70d8405b9a4191130556be29f9fd57a77d9
于平 <yuping.zcp@taobao.com>
2014-08-23 16:30:53 +0800

    硬编码写的"我的"页面，之后会使用Model以及自动以cell

--- TableViewTest/TDViewController.h
+++ TableViewTest/TDViewController.h
@@ -12,7 +12,20 @@
     UITableView *dataTable;
     NSMutableArray *dataArray1;
     NSMutableArray *dataArray2;
+    NSMutableArray *dataArray3;
+    NSMutableArray *dataArray4;
+    NSMutableArray *dataArray5;
     NSMutableArray *titleArray;
+    NSMutableArray *imageArray;
 }
+
+//设置头像距离左边距的长度
+@property (nonatomic,assign) float lineLeftWidth;
+
+
+@property (nonatomic,strong)UIImageView *iconImage;
+@property (nonatomic,strong)UILabel *titleLabel;
+@property (nonatomic,strong)UILabel *subTitleLable;
+@property (nonatomic, strong) UIView * badgeNumberView;
 
 @end
--- TableViewTest/TDViewController.m
+++ TableViewTest/TDViewController.m
@@ -8,7 +8,8 @@
 
 #import "TDViewController.h"
 
-@interface TDViewController ()
+@interface TDViewController (){
+}
 
 @end
 
@@ -18,6 +19,11 @@
 {
     
     [super viewDidLoad];
+    
+    //设置页面标题
+    //[self.view bringSubviewToFront:self.titleLabel];
+    //self.titleLabel.text = @"我的";
+    self.title = @"我的";
 
     //初始化tableView
     //dataTable = [[UITableView alloc] initWithFrame:CGRectMake(2, 0, 320, 420)];//指定位置大小
@@ -25,14 +31,47 @@
     //使得view充满整个屏幕，这个style是分组的
     dataTable = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     [dataTable setDelegate:self];//指定委托
+    
+    
+    
     [dataTable setDataSource:self];//指定数据委托
+    
+    
     //[self.view addSubview:dataTable];
     self.view = dataTable;
     
     //初始化数据
-    dataArray1 = [[NSMutableArray alloc] initWithObjects:@"中国",@"美国",@"英国", nil];
-    dataArray2 = [[NSMutableArray alloc] initWithObjects:@"黄种人",@"黑种人",@"白种人", nil];
-    titleArray = [[NSMutableArray alloc] initWithObjects:@"国家",@"种族", nil];
+    dataArray1 = [[NSMutableArray alloc] initWithObjects:@"堕落天使_于平",@"我的二维码", nil];
+    dataArray2 = [[NSMutableArray alloc] initWithObjects:@"收藏", nil];
+    dataArray3 = [[NSMutableArray alloc] initWithObjects:@"邀请", nil];
+    dataArray4 = [[NSMutableArray alloc] initWithObjects:@"表情盒子", nil];
+    dataArray5 = [[NSMutableArray alloc] initWithObjects:@"设置",@"帮助与反馈", nil];
+    //dataArray2 = [[NSMutableArray alloc] initWithObjects:@"黄种人",@"黑种人",@"白种人", nil];
+    //titleArray = [[NSMutableArray alloc] initWithObjects:@"功能",@"种族", nil];
+    
+    titleArray = [[NSMutableArray alloc] initWithObjects:@"功能",@"收藏",@"邀请",@"表情盒子",@"设置", nil];
+    
+    //初始化图片
+    imageArray = [[NSMutableArray alloc] initWithObjects:@"me",@"myVCQRScanner",@"myVCCollect",@"myVCInviting",@"myEmotionBox",@"myVCSetting",@"myVCFeedBack", nil];
+    
+    
+}
+
+
+
+//为tableview cell增加点击事件，弹出alert
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    /*
+    UIAlertView *alert =[[UIAlertView alloc]initWithTitle:@"提示"
+                                                  message:@"请先登陆"
+                                                 delegate:self
+                                        cancelButtonTitle:@"确定"
+                                        otherButtonTitles:@"取消",nil];
+     */
+    //alert选中的那个cell的数据
+    NSString *titleString = [dataArray1 objectAtIndex:[indexPath row]];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:titleString delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
 }
 
 -(BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation{
@@ -40,7 +79,7 @@
 }
 
 //每个section显示的标题
-
+/*
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
     switch (section) {
         case 0:
@@ -53,32 +92,51 @@
             return @"Unknown";
             break;
     }
+    return [titleArray objectAtIndex:section];
 }
+*/
 
 //指定有多少个分区(Section)，默认为1
 -(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
     return [titleArray count];//返回标题数组中元素的个数来确定分区的个数
+    //return 1;
 }
 
 //指定每个分区有多少行
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-
+    
     switch (section) {
         case 0:
             return [dataArray1 count];
-            break;
         case 1:
             return [dataArray2 count];
-            break;
+        case 2:
+            return [dataArray3 count];
+        case 3:
+            return [dataArray4 count];
+        case 4:
+            return [dataArray5 count];
         default:
             return 0;
-            break;
     }
+    //return [dataArray1 count];
 }
 
+
+
+//设置section之间的间距
+-(CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 20.0;
+}
+
+//指定section的高度
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return 50;
+}
+
+-(CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 2.0;
 }
 
 //绘制cell
@@ -89,24 +147,40 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
     }
+    
     switch (indexPath.section) {
         case 0:
+            [[cell imageView] setImage:[UIImage imageNamed:[imageArray objectAtIndex:indexPath.row]]];
             [[cell textLabel] setText:[dataArray1 objectAtIndex:indexPath.row]];
             break;
         case 1:
+            [[cell imageView] setImage:[UIImage imageNamed:[imageArray objectAtIndex:2]]];
             [[cell textLabel] setText:[dataArray2 objectAtIndex:indexPath.row]];
+            break;
+        case 2:
+            [[cell imageView] setImage:[UIImage imageNamed:[imageArray objectAtIndex:3]]];
+            [[cell textLabel] setText:[dataArray3 objectAtIndex:indexPath.row]];
+            break;
+        case 3:
+            [[cell imageView] setImage:[UIImage imageNamed:[imageArray objectAtIndex:4]]];
+            [[cell textLabel] setText:[dataArray4 objectAtIndex:indexPath.row]];
+            break;
+        case 4:
+            [[cell imageView] setImage:[UIImage imageNamed:[imageArray objectAtIndex: (5 + indexPath.row)]]];
+            
+            [[cell textLabel] setText:[dataArray5 objectAtIndex:indexPath.row]];
             break;
         default:
             [[cell textLabel] setText:@"Unknon"];
             break;
     }
+
     return cell;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
